b2891cac1b3bf1a6e38e63bf9ebbf039c322d952
elvislicsari <elvislicsari@yahoo.com>
2010-11-09 21:24:30 +0200

    clasa orice

---
+++ model/src/com/serveeasy/model/Orice.java
@@ -0,0 +1,11 @@
+package com.serveeasy.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: elvis
+ * Date: 09-Nov-2010
+ * Time: 21:23:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class Orice {
+}
